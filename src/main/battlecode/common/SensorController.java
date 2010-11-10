15fcdfa33a79feb3d44aa89759f5c294c5878317
Daniel Gulotta <dgulotta@alum.mit.edu>
2010-11-09 20:23:39 -0500

    fix sensor generics

--- src/main/battlecode/common/SensorController.java
+++ src/main/battlecode/common/SensorController.java
@@ -3,7 +3,7 @@ package battlecode.common;
 public interface SensorController extends ComponentController {
 
 	public GameObject senseObjectAtLocation(MapLocation loc, RobotLevel height) throws GameActionException;
-	public <T> T [] senseNearbyGameObjects(Class<T> type);
+	public <T extends GameObject> T [] senseNearbyGameObjects(Class<T> type);
 	public MapLocation senseLocationOf(GameObject o) throws GameActionException;
 	public RobotInfo senseRobotInfo(Robot r) throws GameActionException;
 	public MineInfo senseMineInfo(Mine r) throws GameActionException;
--- src/main/battlecode/world/Sensor.java
+++ src/main/battlecode/world/Sensor.java
@@ -32,7 +32,7 @@ public class Sensor extends BaseComponent implements SensorController {
 	}
 
 	@SuppressWarnings("unchecked")
-	public <T> T [] senseNearbyGameObjects(Class<T> type) {
+	public <T extends GameObject> T [] senseNearbyGameObjects(Class<T> type) {
 		Predicate<GameObject> p = Predicates.and(objectWithinRangePredicate(),Predicates.instanceOf(type),Predicates.not(Predicates.equalTo(robot)));
 		return (T [])Iterables.toArray(Iterables.filter(gameWorld.allObjects(),p),GameObject.class);
 	}
