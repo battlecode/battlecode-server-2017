39a97526a11fa52e79b912b6c1f61b13a81ac187
bfrenkel <bfrenkel@sloan.mit.edu>
2011-01-03 22:58:56 +0000

    updated main to support runScrimmage

--- src/main/battlecode/server/Main.java
+++ src/main/battlecode/server/Main.java
@@ -3,12 +3,14 @@ package battlecode.server;
 import java.io.File;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.sql.SQLException;
 
 import battlecode.serial.MatchInfo;
 import battlecode.server.Config;
 import battlecode.server.Server;
 import battlecode.server.ServerFactory;
 import battlecode.server.State;
+import battlecode.util.ScrimmageQueue;
 
 public class Main {
 
@@ -58,12 +60,79 @@ public class Main {
 		}
 	}
 
+	private static void runScrimmage(Config options) {
+
+		String dsn = options.get("bc.server.scrimmage-dsn");
+		String nfs = options.get("bc.server.scrimmage-nfs");
+		System.out.println("dsn in runScrimmage: " + dsn + " dir: " + nfs );
+		if (dsn == null)
+			return;
+
+		ScrimmageQueue queue;
+		try {
+			queue = ScrimmageQueue.connect(dsn);
+		} catch (ClassNotFoundException e2) {
+			e2.printStackTrace();
+			return;
+		} catch (SQLException e2) {
+			e2.printStackTrace();
+			return;
+		}
+
+		try {
+			//poll queue
+			try {
+				while (queue.size() == 0)
+					try {
+						Thread.sleep(30000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+				return;
+			}
+
+			boolean requeue = false;
+			try {
+				Server server = ServerFactory.createSQLServer(options,queue, Server.Mode.MATCH);
+				server.run();
+				if (server.getState() == State.ERROR)
+					requeue = true;
+			} catch (IOException e) {
+				e.printStackTrace();
+				requeue = true;
+			}
+
+			if (requeue) {
+				try {
+					queue.requeue();
+					Thread.sleep(30000);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		} finally {
+			try {
+				queue.close();
+				return;
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	public static boolean run(Config options) {
 		final Server.Mode mode = Server.Mode.valueOf(options.get("bc.server.mode").toUpperCase());
 
 		String saveFile = options.get("bc.server.save-file");	   
 		
 		switch (mode) {
+		case SCRIMMAGE:
+			runScrimmage(options);
+			break;
 		case HEADLESS:
 			runHeadless(options, saveFile);
 			break;
