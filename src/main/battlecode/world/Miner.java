fc6a1f3bf4d4b8d21938a8b452532130944c9cd8
aibolit <aibolit@mit.edu>
2011-01-04 04:04:16 -0500

    mine signals

--- src/main/battlecode/world/Miner.java
+++ src/main/battlecode/world/Miner.java
@@ -2,21 +2,24 @@ package battlecode.world;
 
 import battlecode.common.ComponentType;
 import battlecode.common.RobotLevel;
+import battlecode.world.signal.MineDepletionSignal;
 
 public class Miner extends Builder {
 
-	InternalMine mine;
+    InternalMine mine;
 
-	@SuppressWarnings("unchecked")
-	public Miner(InternalRobot robot) {
-		super(ComponentType.RECYCLER,robot);
-		mine = (InternalMine) gameWorld.getObject(robot.getLocation(),RobotLevel.MINE);
-	}
+    @SuppressWarnings("unchecked")
+    public Miner(InternalRobot robot) {
+        super(ComponentType.RECYCLER, robot);
+        mine = (InternalMine) gameWorld.getObject(robot.getLocation(), RobotLevel.MINE);
+    }
 
-	public void processBeginningOfTurn() {
-		super.processBeginningOfTurn();
-		if(mine!=null)
-			gameWorld.adjustResources(robot.getTeam(),mine.mine());
-	}
+    public void processBeginningOfTurn() {
+        super.processBeginningOfTurn();
+        if (mine != null) {
+            gameWorld.addSignal(new MineDepletionSignal(mine, mine.getRoundsLeft()));
 
+            gameWorld.adjustResources(robot.getTeam(), mine.mine());
+        }
+    }
 }
