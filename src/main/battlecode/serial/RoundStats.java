389e6b3a5da1a564b1e8760ce86e900f4f6ac5da
asugaya <asugaya@mit.edu>
2011-01-10 21:17:13 -0500

    Moar stuff

--- src/main/battlecode/serial/RoundStats.java
+++ src/main/battlecode/serial/RoundStats.java
@@ -1,7 +1,9 @@
 package battlecode.serial;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
+import battlecode.common.ComponentType;
 import battlecode.common.Team;
 
 // Stub for compatibility with last year's Proxy interface.
@@ -10,12 +12,26 @@ public class RoundStats implements Serializable {
     private static final long serialVersionUID = -2422655921465613943L;
     private final double[] gatheredPoints;
     private final double[] points;
+    
+    ArrayList<ComponentType> aTeamComponents;
+    ArrayList<ComponentType> bTeamComponents;
 
-    public RoundStats(double aPoints, double bPoints, double agp, double bgp) {
+    public RoundStats(double aPoints, double bPoints, double agp, double bgp, ArrayList<ComponentType> aComponents, ArrayList<ComponentType> bComponents) {
         points = new double[]{aPoints, bPoints};
         gatheredPoints = new double[]{agp, bgp};
+        aTeamComponents = aComponents;
+        bTeamComponents = bComponents;
     }
-
+    
+    public ArrayList<ComponentType> getATeamComponents(){
+    	return aTeamComponents;
+    }
+    
+    public ArrayList<ComponentType> getBTeamComponents(){
+    	return bTeamComponents;
+    	
+    }
+    
     public double getPoints(Team team) {
         return points[team.ordinal()];
     }
