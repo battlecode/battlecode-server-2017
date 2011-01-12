b8348cc8acad8d3d733c0e00fbf8c6fb0266b91a
Daniel Gulotta <dgulotta@alum.mit.edu>
2011-01-11 19:50:27 -0500

    Don't make enormous match files
    This reverts commit 389e6b3a5da1a564b1e8760ce86e900f4f6ac5da.

--- src/main/battlecode/serial/RoundStats.java
+++ src/main/battlecode/serial/RoundStats.java
@@ -1,9 +1,7 @@
 package battlecode.serial;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
-import battlecode.common.ComponentType;
 import battlecode.common.Team;
 
 // Stub for compatibility with last year's Proxy interface.
@@ -12,26 +10,12 @@ public class RoundStats implements Serializable {
     private static final long serialVersionUID = -2422655921465613943L;
     private final double[] gatheredPoints;
     private final double[] points;
-    
-    ArrayList<ComponentType> aTeamComponents;
-    ArrayList<ComponentType> bTeamComponents;
 
-    public RoundStats(double aPoints, double bPoints, double agp, double bgp, ArrayList<ComponentType> aComponents, ArrayList<ComponentType> bComponents) {
+    public RoundStats(double aPoints, double bPoints, double agp, double bgp) {
         points = new double[]{aPoints, bPoints};
         gatheredPoints = new double[]{agp, bgp};
-        aTeamComponents = aComponents;
-        bTeamComponents = bComponents;
     }
-    
-    public ArrayList<ComponentType> getATeamComponents(){
-    	return aTeamComponents;
-    }
-    
-    public ArrayList<ComponentType> getBTeamComponents(){
-    	return bTeamComponents;
-    	
-    }
-    
+
     public double getPoints(Team team) {
         return points[team.ordinal()];
     }
