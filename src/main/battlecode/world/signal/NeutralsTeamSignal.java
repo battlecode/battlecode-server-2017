dc95577e5546d314f83700bbc46a535d2e058721
Alex X Chen <axcqwerty@gmail.com>
2014-01-15 19:30:32 -0500

    Fix signal

--- src/main/battlecode/world/signal/NeutralsTeamSignal.java
+++ src/main/battlecode/world/signal/NeutralsTeamSignal.java
@@ -18,9 +18,31 @@ public class NeutralsTeamSignal extends Signal {
     private final int[][] teams; // 0 = none, 1 = A, 2 = B, 3 = both
     public NeutralsTeamSignal(InternalObject[] objs, int width, int height) {
         this.teams = new int[width][height];
+        int[][] ids = new int[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                ids[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        for (InternalObject obj : objs) {
+            InternalRobot ir = (InternalRobot) obj;
+            if (ir.type == RobotType.PASTR) {
+                MapLocation[] affected = MapLocation.getAllMapLocationsWithinRadiusSq(ir.getLocation(), GameConstants.PASTR_RANGE);
+                for (MapLocation ml : affected) {
+                    if (ml.x >= 0 && ml.x < width && ml.y >= 0 && ml.y < height) {
+                        ids[ml.x][ml.y] = Math.min(ids[ml.x][ml.y], ir.getID());
+                    }
+                }
+            }
+        }
+
         for (InternalObject obj : objs) {
             InternalRobot ir = (InternalRobot) obj;
             if (ir.type != RobotType.PASTR && ir.type != RobotType.SOLDIER) {
+                continue;
+            }
+            if (ir.type == RobotType.SOLDIER && ids[ir.getLocation().x][ir.getLocation().y] < Integer.MAX_VALUE) {
                 continue;
             }
             int captureRange = 0;
@@ -29,8 +51,8 @@ public class NeutralsTeamSignal extends Signal {
             }
             MapLocation[] affected = MapLocation.getAllMapLocationsWithinRadiusSq(ir.getLocation(), captureRange);
             for (MapLocation ml : affected) {
-                if (ml.x >= 0 && ml.x < width && ml.y >= 0 && ml.y < height) {
-                    teams[ml.x][ml.y] |= (1 << ir.getTeam().ordinal());
+                if (ml.x >= 0 && ml.x < width && ml.y >= 0 && ml.y < height && (ir.type == RobotType.SOLDIER || ir.type == RobotType.PASTR && ir.getID() == ids[ml.x][ml.y])) {
+                    teams[ml.x][ml.y] = (1 << ir.getTeam().ordinal());
                 }
             }
         }
