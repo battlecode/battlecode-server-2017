eab7063cff7ddc0a651bc735a236c4c2391b19ad
Alex X Chen <axcqwerty@gmail.com>
2014-08-03 17:07:03 -0700

    Enable unit tests on battlecode-server

---
+++ src/test/battlecode/common/DirectionTest.java
@@ -0,0 +1,13 @@
+package battlecode.common;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DirectionTest {
+
+    @Test
+    public void testBasic() {
+        Direction dir = Direction.EAST;
+    }
+}
