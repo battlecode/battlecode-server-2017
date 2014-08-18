d27c75558659b49d53b83fee4ca1b187bf388efc
Alex X Chen <axcqwerty@gmail.com>
2014-08-17 22:34:43 -0400

    ADD ROBOT TYPES

--- src/main/battlecode/common/RobotType.java
+++ src/main/battlecode/common/RobotType.java
@@ -6,88 +6,85 @@ import static battlecode.common.RobotLevel.ON_GROUND;
  * Contains details on various attributes of the different robots.
  */
 public enum RobotType {
-    // MXE = max energon / health
-    // SR = sight radius squared
-    // AR = attack radius squared
-    // AD = attack delay
-    // AP = attack power
-    // ATTACK = can attack?
-    // BLDNG = is building?
-    // BUILDTURNS = number of turns required to build
-    //          MXE    SR   AR    AD      AP      ATTACK  BLDNG   BUILDTURNS
-  	HQ          (1000,  35, 24,    2,     24,      true,   false,    0),
-    TOWER       ( 500,  35, 24,    2,     15,      true,   false,    0),
-    SOLDIER     ( 100,  35, 24,    2,    200,      true,   false,    0),
+    //  isbuilding, spawn-source, ore cost, turns cost, supply upkeep, hp, attack, range, movement delay, attack delay, loading delay, cooldown delay, sight range, bytecode limit
+    HQ          (false, null, 0, 0, 0, 1000, 24, 24, 0, 2, 0, 0, 35, 10000),    
+    TOWER       (false, null, 0, 0, 0,  500, 15, 24, 0, 2, 0, 0, 35,  2000),
+
+    SUPPLYDEPOT                 (true, null, 100,  20, 0, 100, 0, 0, 0, 0, 0, 0, 24, 2000),
+    TECHNOLOGYINSTITUTE         (true, null, 300,  80, 0, 100, 0, 0, 0, 0, 0, 0, 24, 2000),
+    BARRACKS                    (true, null, 250,  50, 0, 100, 0, 0, 0, 0, 0, 0, 24, 2000),
+    METABUILDER                 (true, null, 150,  50, 0, 100, 0, 0, 0, 0, 0, 0, 24, 2000),
+    HELIPAD                     (true, null, 250,  50, 0, 100, 0, 0, 0, 0, 0, 0, 24, 2000),
+    TRAININGFIELD               (true, null, 400,  80, 0, 100, 0, 0, 0, 0, 0, 0, 24, 2000),
+    TANKFACTORY                 (true, null, 300,  80, 0, 100, 0, 0, 0, 0, 0, 0, 24, 2000),
+    MINERFACTORY                (true, null, 400, 100, 0, 100, 0, 0, 0, 0, 0, 0, 24, 2000),
+    ENGINEERINGBAY              (true, null, 200, 120, 0, 100, 0, 0, 0, 0, 0, 0, 24, 2000),
+    HANDWASHSTATION             (true, null, 200, 100, 0, 100, 0, 0, 0, 0, 0, 0, 24, 2000),
+    BIOMECHATRONICRESEARCHLAB   (true, null, 200, 200, 0, 100, 0, 0, 0, 0, 0, 0, 24, 2000),
+    AEROSPACELAB                (true, null, 500, 200, 0, 100, 0, 0, 0, 0, 0, 0, 24, 2000),
+
+    FURBY       (false,                  HQ, 100,  20, 20,  30,  6,  8, 2, 2, 1, 1, 24, 10000),
+    COMPUTER    (false, TECHNOLOGYINSTITUTE,  10,  25,  5,   1,  0,  0, 8, 0, 0, 0, 24, 10000),
+    SOLDIER     (false,            BARRACKS,  10,  15, 10,  40,  8, 15, 2, 2, 1, 1, 24, 10000),
+    BASHER      (false,            BARRACKS,  80,  15, 10,  40,  8, 15, 2, 2, 1, 1, 24, 10000),
+    BUILDER     (false,         METABUILDER, 100,  20,  5,  80,  0,  0, 3, 0, 0, 0, 24, 10000),
+    MINER       (false,        MINERFACTORY,  50,  20, 15,  50,  3,  5, 2, 2, 2, 1, 24, 10000),
+    DRONE       (false,             HELIPAD, 120,  40,  5,  80, 10,  8, 1, 3, 0, 0, 24, 10000),
+    TANK        (false,         TANKFACTORY, 200,  60, 30, 160, 20, 24, 2, 3, 2, 2, 24, 10000),
+    COMMANDER   (false,       TRAININGFIELD, 100,  80, 10, 120,  8, 15, 2, 1, 0, 0, 24, 10000),
+    LAUNCHER    (false,        AEROSPACELAB, 400, 100, 50, 400,  0,  0, 4, 0, 0, 0, 24, 10000),
+    MISSILE     (false,            LAUNCHER,   0,   6,  0,   5, 20,  2, 1, 0, 0, 0, 24,   500),
     ;
 
-    /**
-     * The robot's level.
-     */
+    //  isbuilding, spawn-source, ore cost, turns cost, supply upkeep, hp, attack, range, movement delay, attack delay, loading delay, cooldown delay, sight range, bytecode limit
+
     public final RobotLevel level = ON_GROUND;
-
-    /**
-     * The maximum amount of health the robot can have.
-     */
-    public final double maxHealth;
-
-    /**
-     * The square of the maximum distance that the robot can sense.
-     */
-    public final int sensorRadiusSquared;
-
-    /**
-     * The square of the maximum distance that the robot can attack.
-     */
-    public final int attackRadiusSquared;
-
-    /**
-     * Action delay due to attacks. Everything has attack delay 1, except for SOLDIERs, which have an attack delay specified as a game constant (consistent with information here).
-     */
-    public final int attackDelay;
-
-    /**
-     * The amount of damage that this robot does when it attacks.
-     */
-    public final double attackPower;
-
-    /**
-     * Whether or not the robot can attack units.
-     */
-    public final boolean canAttack;
-
-    /**
-     * Whether the robot must be built.
-     */ 
     public final boolean isBuilding;
-
-    /**
-     * How many turns it takes to build this building.
-     */
+    public final RobotType spawnSource;
+    public final int oreCost;
     public final int buildTurns;
+    public final int supplyUpkeep;
+    public final double maxHealth;
+    public final double attackPower;
+    public final int attackRadiusSquared;
+    public final int movementDelay;
+    public final int attackDelay;
+    public final int loadingDelay;
+    public final int cooldownDelay;
+    public final int sensorRadiusSquared;
+    public final int bytecodeLimit;
 
-    /**
-     * Returns true if the robot can attack robots at the given level.
-     */
     public boolean canAttack(RobotLevel level) {
-				return canAttack;
+        return attackPower > 0;
     }
 
-    RobotType(double maxHealth,
-              int sensorRadiusSquared,
-              int attackRadiusSquared,
-              int attackDelay,
+    RobotType(boolean isBuilding,
+              RobotType spawnSource,
+              int oreCost,
+              int buildTurns,
+              int supplyUpkeep,
+              double maxHealth,
               double attackPower,
-              boolean canAttack,
-              boolean isBuilding,
-              int buildTurns) {
-        this.maxHealth = maxHealth;
-        this.sensorRadiusSquared = sensorRadiusSquared;
-        this.attackRadiusSquared = attackRadiusSquared;
-        this.attackDelay = attackDelay;
-        this.attackPower = attackPower;
-        this.canAttack = canAttack;
+              int attackRadiusSquared,
+              int movementDelay,
+              int attackDelay,
+              int loadingDelay,
+              int cooldownDelay,
+              int sensorRadiusSquared,
+              int bytecodeLimit) {
         this.isBuilding = isBuilding;
+        this.spawnSource = spawnSource;
+        this.oreCost = oreCost;
         this.buildTurns = buildTurns;
+        this.supplyUpkeep = supplyUpkeep;
+        this.maxHealth = maxHealth;
+        this.attackPower = attackPower;
+        this.attackRadiusSquared = attackRadiusSquared;
+        this.movementDelay = movementDelay;
+        this.attackDelay = attackDelay;
+        this.loadingDelay = loadingDelay;
+        this.cooldownDelay = cooldownDelay;
+        this.sensorRadiusSquared = sensorRadiusSquared;
+        this.bytecodeLimit = bytecodeLimit;
     }
-
 }
