d1f4d9e6eedfb99ea12274914200c80a86e15604
Alex X Chen <axcqwerty@gmail.com>
2015-01-06 20:32:12 -0500

    Properly fix what the last commits were trying to fix

--- src/main/battlecode/world/RobotControllerImpl.java
+++ src/main/battlecode/world/RobotControllerImpl.java
@@ -173,7 +173,7 @@ public class RobotControllerImpl extends ControllerShared implements RobotContro
         }
        
         for (InternalObject o : gameWorld.allObjects()) {
-            if ((Robot.class.isInstance(o)) && (o.getTeam() == robot.getTeam() && loc.distanceSquaredTo(o.getLocation()) <= sensorRadius)) {
+            if ((Robot.class.isInstance(o)) && (o.getTeam() == robot.getTeam() && loc.distanceSquaredTo(o.getLocation()) <= ((InternalRobot)o).type.sensorRadiusSquared)) {
                 return true;
             }
         }
