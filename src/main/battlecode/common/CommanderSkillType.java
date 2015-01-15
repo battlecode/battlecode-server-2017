9d8f483af870ec4ee5618f06cf71112c91cf7af2
Nathan <npinsker@gmail.com>
2015-01-14 23:49:27 -0500

    tweak CommanderSkillType description wording

--- src/main/battlecode/common/CommanderSkillType.java
+++ src/main/battlecode/common/CommanderSkillType.java
@@ -6,24 +6,24 @@ package battlecode.common;
  * <p>
  * <b>Regeneration</b> - the Commander regenerates 2 hp per turn. This skill is passive and
  * acquired by default. <br>
- * <b>Leadership</b> - all robots within 15 range of the Commander deal 1 additional damage with
- * attacks. This skill is acquired at 500 xp. <br>
  * <b>Flash</b> - The Commander targets a location within range and instantly moves to that location.
- * This skill is acquired at 1000 xp and has a cooldown. <br>
- * <b>Heavy Hands</b> - The commander's attacks deal 4 points of attack and movement delay to the
- * target unit. This skill is acquired at 1500 xp. <br>
+ * This skill is acquired by default and has a cooldown. <br>
+ * <b>Leadership</b> - all robots within 24 range of the Commander deal 1 additional damage with
+ * attacks. This skill is acquired at 1000 xp. The skill becomes twice as effective at 2000 xp. <br>
+ * <b>Heavy Hands</b> - The commander's attacks deal up to 3 points of attack and movement delay
+ * to the target unit. This skill is acquired at 1500 xp. <br>
  */
 public enum CommanderSkillType {
         /* passive: +2 hp/sec */
         REGENERATION,
 
-        /* passive: AoE increase to damage dealt; gained at 500 XP */
+        /* passive: AoE increase to damage dealt; gained at 1000 XP, upgraded at 2000 XP */
         LEADERSHIP,
 
-        /* short-range teleport; gained at 1000 XP */
+        /* short-range teleport */
         FLASH,
 
-        /* attacks deal movement and attack delay */
+        /* attacks deal movement and attack delay; gained at 1500 XP */
         HEAVY_HANDS,
 
         /* ... and more? */
