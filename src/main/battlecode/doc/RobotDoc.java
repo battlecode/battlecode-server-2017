fba85cd377c163d519c750f6eef4ae39aa4a69c1
nmccoy <nmccoy@mit.edu>
2015-08-22 17:06:09 -0500

    Started updating files with new RobotTypes (this will probably take a while to finish)

--- src/main/battlecode/doc/RobotDoc.java
+++ src/main/battlecode/doc/RobotDoc.java
@@ -63,35 +63,42 @@ public class RobotDoc implements Taglet {
         }
         StringBuilder builder = new StringBuilder();
         try {
-            if (rt == RobotType.HQ) {
-                    append(builder, "Type", "HQ");
-            } else if (rt == RobotType.TOWER) {
-                    append(builder, "Type", "Tower");
+            if (rt == RobotType.ARCHON) {
+                    append(builder, "Type", "Archon");
+            } else if (rt == RobotType.ZOMBIEDEN) {
+                    append(builder, "Type", "Zombie Den");
             } else if (rt.isBuilding) {
-                    append(builder, "Type", "Structures"); 
+                    append(builder, "Type", "Structure");
+            } else if (rt.isZombie) {
+            		append(builder, "Type", "Zombie");
             } else {
                     append(builder, "Type", "Unit");
             }
 
-            if (rt.spawnSource != null) {
-                append(builder, "Spawned By", rt.spawnSource.toString());
+            if (rt.isZombie) {
+            	append(builder, "Spawned Automatically By", "Zombie Den");
+            	if (rt.infectTurns > 0) {
+	            	append(builder, "Infect Turns", rt.infectTurns + "");
+	            }
+            } else {
+	            if (rt.spawnSource != null) {
+	                append(builder, "Spawned By", rt.spawnSource.toString());
+	            }
+	            if (rt.partCost > 0) {
+	                append(builder, "Part Cost", rt.partCost + "");
+	            }
+	            if (rt.buildTurns > 0) {
+	                append(builder, "Build Turns", rt.buildTurns + "");
+	            }
+	            if (rt.infectTurns > 0) {
+	            	append(builder, "Infect Turns", rt.infectTurns + "");
+	            }
             }
-            if (rt.dependency != null) {
-                append(builder, "Dependencies", rt.dependency.toString());
-            }
-            if (rt.oreCost > 0) {
-                append(builder, "Ore Cost", rt.oreCost + "");
-            }
-            if (rt.buildTurns > 0) {
-                append(builder, "Build Turns", rt.buildTurns + "");
-            }
-            append(builder, "Supply Upkeep", rt.supplyUpkeep + "");
             append(builder, "Max Health", String.format("%1.0f", rt.maxHealth));
             append(builder, "Attack Power", String.format("%1.1f", rt.attackPower));
             append(builder, "Attack Radius Squared", rt.attackRadiusSquared + "");
             append(builder, "Movement Delay", rt.movementDelay + "");
             append(builder, "Attack Delay", rt.attackDelay + "");
-            append(builder, "Loading Delay", rt.loadingDelay + "");
             append(builder, "Cooldown Delay", rt.cooldownDelay + "");
             append(builder, "Sensor Radius Squared", rt.sensorRadiusSquared + "");
             append(builder, "Bytecode Limit", rt.bytecodeLimit + "");
