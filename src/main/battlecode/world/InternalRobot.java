e52371c9a2b4b2b0528f93d5896b1fb534c78e95
James Gilles <jhgilles@mit.edu>
2015-11-19 16:44:51 -0500

    Move PlayerFactory's static methods to InternalRobot, which is the only class that uses them

--- src/main/battlecode/world/InternalRobot.java
+++ src/main/battlecode/world/InternalRobot.java
@@ -1,9 +1,14 @@
 package battlecode.world;
 
 import battlecode.common.*;
+import battlecode.engine.ErrorReporter;
 import battlecode.engine.GenericRobot;
-import battlecode.engine.PlayerFactory;
+import battlecode.engine.RobotRunnable;
+import battlecode.engine.instrumenter.IndividualClassLoader;
+import battlecode.engine.instrumenter.InstrumentationException;
+import battlecode.engine.scheduler.ScheduledRunnable;
 import battlecode.engine.signal.Signal;
+import battlecode.server.Config;
 import battlecode.world.signal.BroadcastSignal;
 import battlecode.world.signal.DeathSignal;
 import battlecode.world.signal.SpawnSignal;
@@ -118,11 +123,63 @@ public class InternalRobot implements GenericRobot {
         RobotControllerImpl rc = new RobotControllerImpl(gw, this);
 
         if(rc.getTeam() == Team.ZOMBIE){
-            PlayerFactory.loadZombiePlayer(rc);
+            loadZombiePlayer(rc);
         } else {
             String teamName = gw.getTeamName(team);
-            PlayerFactory.loadPlayer(rc, teamName);
+            loadPlayer(rc, teamName);
         }
+    }
+
+    private void loadPlayer(RobotControllerImpl rc, String teamName) {
+        final boolean debugMethodsEnabled = Config.getGlobalConfig().getBoolean("bc.engine.debug-methods");
+
+        // now, we instantiate and instrument the player's class
+        Class playerClass;
+        try {
+            // The classloaders ignore silenced now - RobotMonitor takes care of it
+            ClassLoader icl = new IndividualClassLoader(teamName, debugMethodsEnabled, false, true);
+            playerClass = icl.loadClass(teamName + ".RobotPlayer");
+            //~ System.out.println("PF done loading");
+        } catch (InstrumentationException ie) {
+            // if we get an InstrumentationException, then the error should have been reported, so we just kill the robot
+            System.out.println("[Engine] Error during instrumentation of " + rc.getRobot().toString() + ".\n[Engine] Robot will self-destruct in 3...2...1...");
+            rc.getRobot().suicide();
+            return;
+        } catch (Exception e) {
+            ErrorReporter.report(e);
+            rc.getRobot().suicide();
+            return;
+        }
+
+        // finally, create the player's thread, and let it loose
+        new ScheduledRunnable(new RobotRunnable(playerClass, rc), rc.getRobot().getID());
+
+    }
+
+    private void loadZombiePlayer(RobotControllerImpl rc) {
+        final boolean debugMethodsEnabled = Config.getGlobalConfig().getBoolean("bc.engine.debug-methods");
+
+        // instantiate and instrument the ZombiePlayer class
+        Class playerClass;
+        try {
+            // The classloaders ignore silenced now - RobotMonitor takes care of it
+            ClassLoader icl = new IndividualClassLoader("ZombiePlayer", debugMethodsEnabled, false, true);
+            playerClass = icl.loadClass("ZombiePlayer.ZombiePlayer");
+            //~ System.out.println("PF done loading");
+        } catch (InstrumentationException ie) {
+            // if we get an InstrumentationException, then the error should have been reported, so we just kill the robot
+            System.out.println("[Engine] Error during instrumentation of " + rc.getRobot().toString() + ".\n[Engine] Robot will self-destruct in 3...2...1...");
+            rc.getRobot().suicide();
+            return;
+        } catch (Exception e) {
+            ErrorReporter.report(e);
+            rc.getRobot().suicide();
+            return;
+        }
+
+        // finally, create the ZombiePlayer's thread, and let it loose
+        new ScheduledRunnable(new RobotRunnable(playerClass, rc), rc.getRobot().getID());
+
     }
 
     @Override
