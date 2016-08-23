027fce47452b975c6ce3603a2d3d9851e45fe757
Elijah Stiles <elijahstiles1@gmail.com>
2016-08-22 23:06:36 -0400

    Removed action radius attribute from RobotType (use stride radius instead)

--- src/main/battlecode/common/RobotType.java
+++ src/main/battlecode/common/RobotType.java
@@ -7,56 +7,56 @@ import battlecode.server.Config;
  */
 public enum RobotType {
 
-    // spawnSource, maxHealth, bulletCost, bodyRadius, bulletSpeed, attackPower, actionRadius, sensorRadius, bulletSightRadius, movementDelay, attackDelay, cooldownDelay, bytecodeLimit
+    // spawnSource, maxHealth, bulletCost, bodyRadius, bulletSpeed, attackPower, sensorRadius, bulletSightRadius, movementDelay, attackDelay, cooldownDelay, bytecodeLimit
     /**
      * An important unit that cannot be constructed; builds other robots.
      *
      * @battlecode.doc.robottype
      */
-    ARCHON          (null,    1000,   -1,   2,  -1,  -1,   3,   7,  15,   2,  -1,  1, 20000),
-    //                          HP    BC   BR   BS   AP   AR   SR  BSR  MVD   AD   CD    BCL
+    ARCHON          (null,    1000,   -1,   2,  -1,  -1,   7,  15,   2,  -1,   1, 20000),
+    //                          HP    BC   BR   BS   AP   SR  BSR  MVD   AD   CD    BCL
     /**
      * The main producer unit to make other units and trees; can't build Archons or other Gardeners
      *
      * @battlecode.doc.robottype
      */
-    GARDENER        (ARCHON,   100,  100,   1,  -1,  -1,   3,   5,  10,   2,  -1,   1, 10000),
-    //                          HP    BC   BR   BS   AP   AR   SR  BSR  MVD   AD   CD    BCL
+    GARDENER        (ARCHON,   100,  100,   1,  -1,  -1,   5,  10,   2,  -1,   1, 10000),
+    //                          HP    BC   BR   BS   AP   SR  BSR  MVD   AD   CD    BCL
     /**
      * A melee based unit that specializes at cutting down trees
      *
      * @battlecode.doc.robottype
      */
-    LUMBERJACK      (GARDENER,  70,  100,   1,  -1, 1.5F,   2,   5,  10,   2,   2,   1, 10000),
-    //                          HP    BC   BR   BS   AP   AR   SR  BSR  MVD   AD   CD    BCL
+    LUMBERJACK      (GARDENER,  70,  100,   1,  -1, 1.5F,   5,  10,   2,   2,   1, 10000),
+    //                          HP    BC   BR   BS   AP    SR  BSR  MVD   AD   CD    BCL
     /**
      * The basic fighting unit
      *
      * @battlecode.doc.robottype
      */
-    RECRUIT         (GARDENER,  70,  100,   1,   1,   1,   1,   5,  10,   1,   2,   1, 10000),
-    //                          HP    BC   BR   BS   AP   AR   SR  BSR  MVD   AD   CD    BCL
+    RECRUIT         (GARDENER,  70,  100,   1,   1,   1,   5,  10,   1,   2,   1, 10000),
+    //                          HP    BC   BR   BS   AP   SR  BSR  MVD   AD   CD    BCL
     /**
      * A slightly better fighting unit
      *
      * @battlecode.doc.robottype
      */
-    SOLDIER         (GARDENER,  80,  130,   1,   1,   1,   1,   5,  10,   1,   2,   1, 10000),
-    //                          HP    BC   BR   BS   AP   AR   SR  BSR  MVD   AD   CD    BCL
+    SOLDIER         (GARDENER,  80,  130,   1,   1,   1,   5,  10,   1,   2,   1, 10000),
+    //                          HP    BC   BR   BS   AP   SR  BSR  MVD   AD   CD    BCL
     /**
      * A strong fighting unit
      *
      * @battlecode.doc.robottype
      */
-    TANK            (GARDENER, 125,  200,   1,   1,   1,   1,   5,  10,   1,   2,   1, 10000),
-    //                          HP    BC   BR   BS   AP   AR   SR  BSR  MVD   AD   CD    BCL
+    TANK            (GARDENER, 125,  200,   1,   1,   1,   5,  10,   1,   2,   1, 10000),
+    //                          HP    BC   BR   BS   AP   SR  BSR  MVD   AD   CD    BCL
     /**
      * An unit that specializes in movement
      *
      * @battlecode.doc.robottype
      */
-    SCOUT           (GARDENER,  80,   80,   1,   1,   1,   1,   5,  10,   1,   2,   1, 20000),
-    //                          HP    BC   BR   BS   AP   AR   SR  BSR  MVD   AD   CD    BCL
+    SCOUT           (GARDENER,  80,   80,   1,   1,   1,   5,  10,   1,   2,   1, 20000),
+    //                          HP    BC   BR   BS   AP   SR  BSR  MVD   AD   CD    BCL
     ;
     
     /**
@@ -88,11 +88,6 @@ public enum RobotType {
      * Base damage per attack.
      */
     public final float attackPower;
-
-    /**
-     * Range for performing actions.
-     */
-    public final int actionRadius;
 
     /**
      * Range for sensing robots and trees.
@@ -170,7 +165,7 @@ public enum RobotType {
     }
     
     RobotType(RobotType spawnSource, int maxHealth, int bulletCost, int bodyRadius, int bulletSpeed, float attackPower,
-              int actionRadius, int sensorRadius, int bulletSightRadius, float movementDelay, float attackDelay,
+              int sensorRadius, int bulletSightRadius, float movementDelay, float attackDelay,
               float cooldownDelay, int bytecodeLimit) {
         this.spawnSource        = spawnSource;
         this.maxHealth          = maxHealth;
@@ -178,12 +173,15 @@ public enum RobotType {
         this.bodyRadius         = bodyRadius;
         this.bulletSpeed        = bulletSpeed;
         this.attackPower        = attackPower;
-        this.actionRadius       = actionRadius;
         this.sensorRadius       = sensorRadius;
         this.bulletSightRadius  = bulletSightRadius;
         this.movementDelay      = movementDelay;
         this.attackDelay        = attackDelay;
         this.cooldownDelay      = cooldownDelay;
         this.bytecodeLimit      = bytecodeLimit;
+    }
+
+    public float getStrideRadius(){
+        return 2*bodyRadius;
     }
 }
