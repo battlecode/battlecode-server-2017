edbedd7f7c6e18e3c5d0951004f62a859240079b
Jamie Bloxham <jamie.a.bloxham@gmail.com>
2017-01-10 16:12:35 -0500

    is it over yet

--- src/main/battlecode/common/BodyInfo.java
+++ src/main/battlecode/common/BodyInfo.java
@@ -6,31 +6,43 @@ package battlecode.common;
 public interface BodyInfo {
 
     /**
-     * @return the id of the body.
+     * Returns the ID of this body.
+     *
+     * @return the ID of this body.
      */
     int getID();
 
     /**
+     * Returns the center location of this body.
+     *
      * @return the center location of this body.
      */
     MapLocation getLocation();
 
     /**
+     * Returns the radius of this body.
+     *
      * @return the radius of this body.
      */
     float getRadius();
 
     /**
+     * Returns whether this body is a robot.
+     *
      * @return true if this body is a robot; false otherwise.
      */
     boolean isRobot();
 
     /**
+     * Returns whether this body is a tree.
+     *
      * @return true if this body is a tree; false otherwise.
      */
     boolean isTree();
 
     /**
+     * Returns whether this body is a bullet.
+     *
      * @return true if this body is a bullet; false otherwise.
      */
     boolean isBullet();
