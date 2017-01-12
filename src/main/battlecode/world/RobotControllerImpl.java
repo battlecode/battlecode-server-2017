3be1a0b17c3962609355e47f07020d7eefb00160
Gina Yuan <gina.yuan@gmail.com>
2017-01-12 17:43:05 -0500

    Typo

--- src/main/battlecode/world/RobotControllerImpl.java
+++ src/main/battlecode/world/RobotControllerImpl.java
@@ -896,7 +896,7 @@ public final strictfp class RobotControllerImpl implements RobotController {
     private void shakeTree(InternalTree tree){
         this.robot.incrementShakeCount();
         if (tree.getContainedBullets() > 0) {
-            gameWorld.getGameMaker().addShaken(tree.getID());
+            gameWorld.getMatchMaker().addShaken(tree.getID());
         }
         gameWorld.getTeamInfo().adjustBulletSupply(getTeam(), tree.getContainedBullets());
         tree.resetContainedBullets();
